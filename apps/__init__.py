0afe187b0b20d6022c2217aa5bb2b9894a186aa7
rashwalt <foxbland@gmail.com>
2015-10-14 18:36:52 +0900

    もろもろ修正した

---
+++ apps/__init__.py
@@ -0,0 +1,8 @@
+# default global setting
+import os
+
+from django.template.base import add_to_builtins
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stm.settings")
+
+add_to_builtins('bootstrap3.templatetags.bootstrap3')
--- stm/settings/base.py
+++ stm/settings/base.py
@@ -17,10 +17,10 @@ PROJECT_ROOT = dirname(DJANGO_ROOT)
 SITE_NAME = basename(DJANGO_ROOT)
 
 # Collect static files here
-STATIC_ROOT = join(PROJECT_ROOT, 'run', 'static')
+STATIC_ROOT = join(PROJECT_ROOT, 'static')
 
 # Collect media files here
-MEDIA_ROOT = join(PROJECT_ROOT, 'run', 'media')
+MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 
 # look for static assets here
 STATICFILES_DIRS = [
@@ -39,18 +39,17 @@ sys.path.append(normpath(join(PROJECT_ROOT, 'apps')))
 
 # ##### APPLICATION CONFIGURATION #########################
 
-# This are the apps
-DEFAULT_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
 
 # Middlewares
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,7 +58,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-]
+)
 
 # Template stuff
 TEMPLATES = [
@@ -69,12 +68,13 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
+                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages'
             ],
         },
@@ -84,9 +84,8 @@ TEMPLATES = [
 
 # ##### SECURITY CONFIGURATION ############################
 
-# We store the secret key here
-# The required SECRET_KEY is fetched at the end of this file
-SECRET_FILE = normpath(join(PROJECT_ROOT, 'run', 'SECRET.key'))
+# secret key here
+SECRET_KEY = 'j9+4_o9mvef8_kv&ono&0&f-v&)9qk_49@$o275198mo$q4him'
 
 # These persons receive error notification
 ADMINS = (
@@ -98,10 +97,10 @@ MANAGERS = ADMINS
 # ##### DJANGO RUNNING CONFIGURATION ######################
 
 # The default WSGI application
-WSGI_APPLICATION = '%s.wsgi.application' % SITE_NAME
+WSGI_APPLICATION = '{}.wsgi.application'.format(SITE_NAME)
 
 # The root URL configuration
-ROOT_URLCONF = '%s.urls' % SITE_NAME
+ROOT_URLCONF = '{}.urls'.format(SITE_NAME)
 
 # This site's ID
 SITE_ID = 1
@@ -116,11 +115,13 @@ MEDIA_URL = '/media/'
 # ##### DEBUG CONFIGURATION ###############################
 DEBUG = False
 
+ALLOWED_HOSTS = []
+
 
 # ##### INTERNATIONALIZATION ##############################
 
-LANGUAGE_CODE = 'de'
-TIME_ZONE = 'Europe/Berlin'
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'UTC'
 
 # Internationalization
 USE_I18N = True
@@ -132,15 +133,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Finally grab the SECRET KEY
-try:
-    SECRET_KEY = open(SECRET_FILE).read().strip()
-except IOError:
-    try:
-        from django.utils.crypto import get_random_string
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!$%&()=+-_'
-        SECRET_KEY = get_random_string(50, chars)
-        with open(SECRET_FILE, 'w') as f:
-            f.write(SECRET_KEY)
-    except IOError:
-        raise Exception('Could not open %s for writing!' % SECRET_FILE)
+# ##### SECRET ############################################
+
+
